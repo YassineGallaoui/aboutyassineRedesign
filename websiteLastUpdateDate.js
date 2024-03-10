678b903772e533a637b5cd2b0e246c0463549e79
Yassine <22yassine05@gmail.com>
2024-03-10 19:24:05 +0100

    setted static "last edit date"

--- websiteLastUpdateDate.js
+++ websiteLastUpdateDate.js
@@ -2,7 +2,8 @@ let websiteLastUpdateDate = {};
 
 websiteLastUpdateDate.getLastUpdateDate = function () {
   const options = { day: "numeric", month: "short", year: "numeric" };
-  return new Date().toLocaleDateString("en-US", options);
+  let date = new Date().toLocaleDateString("en-US", options);
+  return "Mar 10, 2024";
 };
 
 module.exports = websiteLastUpdateDate;
