92b21375049f443cd25a0f5e0b3bf28040d21e33
Yassine <22yassine05@gmail.com>
2024-03-11 13:40:46 +0100

    removed generated date

--- websiteLastUpdateDate.js
+++ websiteLastUpdateDate.js
@@ -2,7 +2,6 @@ let websiteLastUpdateDate = {};
 
 websiteLastUpdateDate.getLastUpdateDate = function () {
   const options = { day: "numeric", month: "short", year: "numeric" };
-  let date = new Date().toLocaleDateString("en-US", options);
   return "Mar 10, 2024";
 };
 
