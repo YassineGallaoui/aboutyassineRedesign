f180a3bcde6f72b285e62ba79829e0a22833db16
YassineGallaoui <22yassine05@gmail.com>
2024-11-27 23:16:28 +0100

    improved carousel and crt screen

--- components/RotateDevice/index.tsx
+++ components/RotateDevice/index.tsx
@@ -5,9 +5,12 @@ import { RotateDeviceIcon } from "./RotateDeviceIcon";
 
 export const rotateDeviceDuration = 0.3;
 export const RotateDevice = ({ showComponent, animationFinished }) => {
+
   useEffect(() => {
-    const timer = setTimeout(() => { animationFinished(false) }, rotateDeviceDuration * 1000 * 6);
-    return () => clearTimeout(timer);
+    if (showComponent) {
+      const timer = setTimeout(() => { animationFinished(false) }, rotateDeviceDuration * 1000 * 6);
+      return () => clearTimeout(timer);
+    }
   }, [showComponent]);
 
   const dcVariants = {
@@ -19,13 +22,20 @@ export const RotateDevice = ({ showComponent, animationFinished }) => {
       }
     },
     initial: {
+      zIndex: [-1],
+      opacity: [0],
+      transition: {
+        duration: 0.3,
+      }
+    }
+    /* initial: {
       zIndex: [3, 3, -1],
       opacity: [1, 0, 0],
       transition: {
         duration: 0.3,
         times: [0, 0.99, 1],
       }
-    }
+    } */
   }
 
   return (
